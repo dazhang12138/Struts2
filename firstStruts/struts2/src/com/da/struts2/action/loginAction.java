a203e188b3748b9866bbb3e4a02309b8266d9f8e
dazhang12138 <836160973da@gmail.com>
2016-12-24 22:26:10 +0800

    firstStruts2
    小白写的第一个java Web Struts框架

---
+++ firstStruts/struts2/src/com/da/struts2/action/loginAction.java
@@ -0,0 +1,10 @@
+package com.da.struts2.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
+public class loginAction extends ActionSupport {
+	public String login() throws Exception{
+		return "success";
+	}
+}
